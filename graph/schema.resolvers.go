3239471013dd1aa184d6f988412da86a7a934962
YutaUra <yuuta3594@outlook.jp>
2022-06-05 13:37:08 +0000

    CRUD

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -6,17 +6,55 @@ package graph
 import (
 	"context"
 	"fmt"
+	"math/rand"
 
 	"github.com/YutaUra/go-graphql-server/graph/generated"
 	"github.com/YutaUra/go-graphql-server/graph/model"
 )
 
-func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
+func (r *mutationResolver) CreateTodo(ctx context.Context, input model.CreateTodoInput) (*model.Todo, error) {
+	todo := &model.Todo{
+		Text: input.Text,
+		ID:   fmt.Sprintf("T%d", rand.Int()),
+		User: &model.User{ID: input.UserID, Name: "user " + input.UserID},
+	}
+	r.todos = append(r.todos, todo)
+	return todo, nil
+}
+
+func (r *mutationResolver) UpdateTodo(ctx context.Context, input model.UpdateTodoInput) (*model.Todo, error) {
+	for _, todo := range r.todos {
+		if todo.ID != input.ID {
+			continue
+		}
+		if input.Done != nil {
+			todo.Done = *input.Done
+		}
+		if input.Text != nil {
+			todo.Text = *input.Text
+		}
+		return todo, nil
+	}
+	return nil, fmt.Errorf("Todo is not found")
+}
+
+func (r *mutationResolver) DeleteTodo(ctx context.Context, input model.DeleteTodoInput) (*model.Todo, error) {
+	for i, todo := range r.todos {
+		if todo.ID != input.ID {
+			continue
+		}
+		r.todos = append(r.todos[:i], r.todos[i+1:]...)
+		return todo, nil
+	}
+	return nil, fmt.Errorf("Todo is not found")
 }
 
 func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
+	return r.todos, nil
+}
+
+func (r *todoResolver) User(ctx context.Context, obj *model.Todo) (*model.User, error) {
+	return &model.User{ID: obj.UserID, Name: "user " + obj.UserID}, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
@@ -25,5 +63,9 @@ func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResol
 // Query returns generated.QueryResolver implementation.
 func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
+// Todo returns generated.TodoResolver implementation.
+func (r *Resolver) Todo() generated.TodoResolver { return &todoResolver{r} }
+
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
+type todoResolver struct{ *Resolver }
